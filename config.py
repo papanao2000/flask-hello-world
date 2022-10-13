72da5e376b453d769ff82795070847b214cb7dbd
miho777 <naozumi.miho@emc.com>
2022-10-13 16:30:55 +0900

    forLab07

---
+++ config.py
@@ -0,0 +1,7 @@
+ecs_test_drive = {
+    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
+    'ecs_access_key_id' : '132054832271360440@ecstestdrive.emc.com',
+    'ecs_secret_key' : 'pJVPGgPEIzHGwBdUhuBNxThjiyE4MTbAggaRfD46',
+    'ecs_bucket_name' : 'photo-album'
+}
+### for NM account
